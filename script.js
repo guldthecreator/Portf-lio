ad0a5ed0cdd71d085d01fb0b79a5fced9b87c4c3
guldthecreator <g.dantas@ba.estudante.senai.br>
2024-10-28 02:19:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+});
